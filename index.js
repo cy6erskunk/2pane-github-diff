abfcacd665ad33a51500945cde3027c60bbab726
Igor Shevchenko <cyberskunk@gmail.com>
2014-04-26 20:50:31 +0400

    single page is enough

--- index.js
+++ index.js
@@ -6,47 +6,50 @@ var express = require('express'),
     diffGetter  = require('./lib/diff-getter'),
     fs = require('fs'),
     equal = require('deep-equal'),
-    renderDiff = function (req, res) {
-        return res.render('diff.jade', {});
-    },
-    ignoreCache = Boolean(process.env.IGNORE_CACHE);
+    ignoreCache = Boolean(process.env.IGNORE_CACHE),
+    options = {};
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/', function (req, res) {
-    res.render('index.jade', {});
-});
+app.get('/', function (req, res, next) {
+    var cacheConf = 'cache.json';
 
-app.get('/retrieve', function (req, res, next) {
-    var cacheConf = 'cache.json',
-        options = {
-            username: req.query.user,
-            repo: req.query.repo,
-            filename: req.query.filename,
-            commitish: req.query.commit || null
-        };
+    options = {
+        username: req.query.user || null,
+        repo: req.query.repo || null,
+        filename: req.query.filename || null,
+        commitish: req.query.commit || null
+    };
 
     if (! ignoreCache && fs.existsSync(cacheConf) && equal(options, JSON.parse(fs.readFileSync(cacheConf)))) {
         debug('loading from cache, options: ', options);
         return next();
     }
 
-    diffGetter(options).then(function (response) {
-        var patch = 'var diff_model = ' + response.patch + ';\n\n',
-            fileBefore = 'var content_before = "' + jsesc(response.fileBefore, { quotes: 'double'}) + '";\n\n',
-            fileAfter = 'var content_after = "' + jsesc(response.fileAfter, { quotes: 'double'}) + '";\n\n';
+    if (options.username && options.repo && options.filename) {
+        diffGetter(options).then(function (response) {
+            var patch = 'var diff_model = ' + response.patch + ';\n\n',
+                fileBefore = 'var content_before = "' + jsesc(response.fileBefore, { quotes: 'double'}) + '";\n\n',
+                fileAfter = 'var content_after = "' + jsesc(response.fileAfter, { quotes: 'double'}) + '";\n\n';
 
-        fs.writeFileSync('dist/diff_test.js', patch + fileBefore + fileAfter);
-        fs.writeFileSync(cacheConf, JSON.stringify(options));
-        next();
-    }).fail(function (error) {
-        return next(error);
+            fs.writeFileSync('dist/diff_test.js', patch + fileBefore + fileAfter);
+            fs.writeFileSync(cacheConf, JSON.stringify(options));
+        }).fail(function (error) {
+            return next(error);
+        });
+    } else {
+        options.empty = true;
+    }
+    return next();
+}, function (req, res) {
+    res.render('diff.jade', {
+        user: options.username || '',
+        repo: options.repo || '',
+        file: options.filename || '',
+        commit: options.commitish || '',
+        empty: Boolean(options.empty)
     });
-}, function (req, res, next) {
-    renderDiff(req, res);
 });
-
-app.get('/diff', renderDiff);
 
 app.use(function (err, req, res, next) {
     console.log(err.stack);
