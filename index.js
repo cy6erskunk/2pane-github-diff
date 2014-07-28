44b60810255c47a8256d1ffbd3aa00e5c9330ded
Igor Shevchenko <cyberskunk@gmail.com>
2014-07-28 19:58:45 +0400

    minor fix; kinda 404

--- index.js
+++ index.js
@@ -32,53 +32,57 @@ app.get('/', function (req, res, next) {
         if (req.cookies.gh_access_token) {
 
             return VowGithub.getUser(req.cookies.gh_access_token)
-            .then(function(response) {
-                var data = JSON.parse(response.data);
-                if (data.error) {
-                    return next(data.error);
-                } else {
-                    if ( ! req.cookies.gh_login || req.cookies.gh_login == data.login) {
-                        res.cookie('gh_login', data.login);
-                        options.gh_login = data.login;
-                    } else if (req.cookies.gh_login != data.login) {
+                .then(function(response) {
+                    var data = JSON.parse(response.data);
+                    if (data.error) {
+                        return next(data.error);
+                    } else {
+                        if ( ! req.cookies.gh_login || req.cookies.gh_login == data.login) {
+                            res.cookie('gh_login', data.login);
+                            options.gh_login = data.login;
+                        } else if (req.cookies.gh_login != data.login) {
+                            res.clearCookie('gh_access_token');
+                            res.clearCookie('gh_login');
+                        }
+                    }
+                }, function(error) {
+                    if (error.data.statusCode == 401) {
                         res.clearCookie('gh_access_token');
                         res.clearCookie('gh_login');
+                    } else {
+                        return next(error);
                     }
-                }
-            }, function(error) {
-                if (error.data.statusCode == 401) {
-                    res.clearCookie('gh_access_token');
-                    res.clearCookie('gh_login');
-                } else {
-                    return next(error);
-                }
-            });
+                });
         }
     })
     .always(function() {
+        fs.writeFileSync('dist/diff_test.js', 'var diff_model = [], content_before = content_after = \'\';');
+
         if (! ignoreCache && fs.existsSync(cacheConf) && equal(options, JSON.parse(fs.readFileSync(cacheConf)))) {
             debug('loading from cache, options: ', options);
             return next();
         }
-
         if (options.user && options.repo && options.file) {
-            fs.writeFileSync('dist/diff_test.js', 'var diff_model = [], content_before = content_after = \'\';');
-
             diffGetter(options)
                 .then(function (response) {
-                    var patch = 'diff_model = ' + response.patch + ';\n\n',
-                        fileBefore = 'content_before = "' + jsesc(response.fileBefore, { quotes: 'double'}) + '";\n\n',
-                        fileAfter = 'content_after = "' + jsesc(response.fileAfter, { quotes: 'double'}) + '";\n\n';
+                    var patch,
+                        fileBefore,
+                        fileAfter;
+
+                    patch = 'diff_model = ' + response.patch + ';\n\n',
+                    fileBefore = 'content_before = "' + jsesc(response.fileBefore, { quotes: 'double'}) + '";\n\n',
+                    fileAfter = 'content_after = "' + jsesc(response.fileAfter, { quotes: 'double'}) + '";\n\n';
 
                     fs.writeFileSync('dist/diff_test.js', patch + fileBefore + fileAfter);
                     fs.writeFileSync(cacheConf, JSON.stringify(options));
+                    return next();
                 }).fail(function (error) {
                     return next(error);
                 });
         } else {
             options.empty = true;
+            return next();
         }
-        return next(error);
     });
 }, function (req, res) {
     res.render('diff.jade', options);
@@ -103,8 +107,12 @@ app.get('/callback', function (req, res, next) {
 });
 
 app.use(function (err, req, res, next) {
-    console.log(err.stack);
-    res.send(500, 'WHOOPS...');
+    if (err.data && err.data.statusCode && err.data.statusCode === 404) {
+        res.send(404, 'kinda 404 :( <br/><a href="/">Go Home and try again</a>');
+    } else {
+        console.log(err.stack);
+        res.send(500, 'WHOOPS...');
+    }
 });
 
 console.log('starting on port ' + (Number(process.env.PORT) || config.port));
